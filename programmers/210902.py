8463b3239d46872b826f98a753b7047e34622c2f
Nahyun Cho <w2038@e-mirim.hs.kr>
2021-09-02 16:38:37 +0900

    9월 2일 Commit

---
+++ programmers/210902.py
@@ -0,0 +1,10 @@
+# 행렬의 덧셈
+
+def solution(arr1, arr2):
+    answer = []
+    for x in range(len(arr1)):
+        tmp = []
+        for y in range(len(arr1[x])):
+            tmp.append(arr1[x][y] + arr2[x][y])
+        answer.append(tmp)
+    return answer
