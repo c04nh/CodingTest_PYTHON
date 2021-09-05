1e4a704e1735d0989bb8f6c9f5d6c9821cbe49b0
Nahyun Cho <w2038@e-mirim.hs.kr>
2021-09-05 21:53:38 +0900

    9월 5일 Commit

---
+++ programmers/210905.py
@@ -0,0 +1,11 @@
+# 문자열을 정수로 바꾸기
+
+def solution(s):
+    answer = 0
+    if s[0] == '+':
+        answer = int(s[1:])
+    elif s[0] == '-':
+        answer = -1*int(s[1:])
+    else:
+        answer = int(s)
+    return answer
