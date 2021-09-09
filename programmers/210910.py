cac93d87a98289c87f3c4c4a5fbe69317fa6c9f8
Nahyun Cho <w2038@e-mirim.hs.kr>
2021-09-10 08:37:25 +0900

    9월 10일 Commit

---
+++ programmers/210910.py
@@ -0,0 +1,10 @@
+# 문자열 내 p와 y의 개수
+
+def solution(s):
+    p = s.count('p') + s.count('P')
+    y = s.count('y') + s.count('Y')
+
+    if p == y:
+        return True
+    else:
+        return False
