af0e3c2e3fe18ad9affa6799fcf51bbc72d2987c
Nahyun Cho <w2038@e-mirim.hs.kr>
2021-09-12 01:41:04 +0900

    9월 12일 Commit

---
+++ programmers/210912.py
@@ -0,0 +1,10 @@
+# 나누어 떨어지는 숫자 배열
+
+def solution(arr, divisor):
+    answer = []
+    for a in arr:
+        if a % divisor == 0:
+            answer.append(a)
+    if len(answer) == 0:
+        answer.append(-1)
+    return sorted(answer)
