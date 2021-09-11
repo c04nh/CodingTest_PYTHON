ab8a8f74d13672c04f5ba520f3f406f2d05ba770
Nahyun Cho <w2038@e-mirim.hs.kr>
2021-09-11 11:41:57 +0900

    9월 11일 Commit

---
+++ programmers/210911.py
@@ -0,0 +1,13 @@
+# 두 정수 사이의 합
+
+def solution(a, b):
+    answer = 0
+    if a < b:
+        for x in range(a, b + 1):
+            answer += x
+    elif a > b:
+        for x in range(b, a + 1):
+            answer += x
+    else:
+        return a
+    return answer
