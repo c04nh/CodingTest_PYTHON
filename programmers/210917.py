ad9afe7fe7db2838b9be1c056af6462a4d891bba
Nahyun Cho <w2038@e-mirim.hs.kr>
2021-09-17 10:44:39 +0900

    9월 17일 Commit

---
+++ programmers/210917.py
@@ -0,0 +1,15 @@
+# K번째수
+
+def solution(array, commands):
+    answer = []
+    for a in commands:
+        arr = []
+        i = a[0] - 1
+        j = a[1] - 1
+        k = a[2] - 1
+        for b in range(i, j + 1):
+            arr.append(array[b])
+        arr.sort()
+        answer.append(arr[k])
+
+    return answer
