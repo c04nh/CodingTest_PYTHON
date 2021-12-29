f9b7234b7b0d5001b7135acd2d57c5167081c537
c04nh <w2038@e-mirim.hs.kr>
2021-12-29 10:42:08 +0900

    12월 29일 Commit

---
+++ Baekjoon/211229.py
@@ -0,0 +1,9 @@
+# A+B - 8
+
+num = int(input())
+for a in range(1, num + 1):
+    user_input = input()
+    arr = user_input.split(' ')
+    A = int(arr[0])
+    B = int(arr[1])
+    print(f'Case #{a}: {A} + {B} = {A + B}')
