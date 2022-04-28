ec9e9f83fcb57672b9f3d8ade6c10ee85a279b01
c04nh <w2038@e-mirim.hs.kr>
2022-04-28 19:15:17 +0900

    4월 28일 Commit

--- Baekjoon/220428.py
+++ Baekjoon/220428.py
@@ -1,13 +1,33 @@
-# 오큰수
+# 큐 2
 
 import sys
-N = int(input())
-A = list(map(int, sys.stdin.readline().split()))
-answer = [-1] * N
-stack = []
-stack.append(0)
-for i in range(1, N):
-    while stack and A[stack[-1]] < A[i]:
-        answer[stack.pop()] = A[i]
-    stack.append(i)
-print(*answer)
+from collections import deque
+
+n = int(sys.stdin.readline())
+q = deque([])
+for i in range(n):
+    s = sys.stdin.readline().split()
+    if s[0] == 'push':
+        q.append(s[1])
+    elif s[0] == 'pop':
+        if not q:
+            print(-1)
+        else:
+            print(q.popleft())
+    elif s[0] == 'size':
+        print(len(q))
+    elif s[0] == 'empty':
+        if not q:
+            print(1)
+        else:
+            print(0)
+    elif s[0] == 'front':
+        if not q:
+            print(-1)
+        else:
+            print(q[0])
+    elif s[0] == 'back':
+        if not q:
+            print(-1)
+        else:
+            print(q[-1])
