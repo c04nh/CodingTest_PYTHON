b515802e5eaccc2434166bc5028614a3d496b208
c04nh <w2038@e-mirim.hs.kr>
2022-05-23 21:17:29 +0900

    5월 23일 Commit

---
+++ programmers/220523.py
@@ -0,0 +1,16 @@
+# 2단계
+# 타겟 넘버
+
+def solution(numbers, target):
+    answer = 0
+    leaves = [0]
+    for num in numbers:
+        tmp = []
+        for parent in leaves:
+            tmp.append(parent + num)
+            tmp.append(parent - num)
+        leaves = tmp
+    for leaf in leaves:
+        if leaf == target:
+            answer += 1
+    return answer
