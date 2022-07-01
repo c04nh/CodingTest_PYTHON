826fffb39fd83a2dbdb64ba901f39011634c4563
c04nh <w2038@e-mirim.hs.kr>
2022-07-01 20:00:35 +0900

    7월 1일 Commit

---
+++ programmers/220701.py
@@ -0,0 +1,9 @@
+# 2단계
+# H-index
+
+def solution(citations):
+    citations.sort()
+    for idx , citation in enumerate(citations):
+        if citation >= len(citations) - idx :
+            return len(citations) - idx
+    return 0
