c0d747cf9cd214eb4b451b7111d254f31de3c2b8
nhsally <nhsally@naver.com>
2022-07-04 14:02:42 +0900

    7월 4일 Commit

---
+++ programmers/220704.py
@@ -0,0 +1,10 @@
+# 2단계
+# 주식가격
+
+def solution(prices):
+    answer = [0] * len(prices)
+    for i in range(0, len(prices)):
+        for j in range(i + 1, len(prices)):
+            answer[i] += 1
+            if prices[i] > prices[j]: break
+    return answer
