30ac4287c858e105c23123c4ffbcd4710a0a6237
nhsally <nhsally@naver.com>
2022-07-08 08:23:44 +0900

    7월 8일 Commit

---
+++ programmers/220708.py
@@ -0,0 +1,13 @@
+# 2단계
+# 큰 수 만들기
+
+def solution(number, k):
+    answer = []
+
+    for num in number:
+        while k > 0 and answer and answer[-1] < num:
+            answer.pop()
+            k -= 1
+        answer.append(num)
+
+    return ''.join(answer[:len(answer) - k])
